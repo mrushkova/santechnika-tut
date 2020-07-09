1550c69849df36aa3a34df31db3223030cae7b5f
Maria Rushkova <mariaruskova@Marias-MacBook.local>
2020-07-09 23:16:47 +0200

    Блоки спецпредложения, инфоблоки

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,31 @@
 $(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
+  $('.header__down').owlCarousel({
     loop: true, // Зацикливаем слайдер
     margin: 0, // Отступ от элемента справа в 50px
     nav: false, // Отключение навигации
     autoplay: true, // Автозапуск слайдера
-    smartSpeed: 2000, // Время движения слайда
-    autoplayTimeout: 4000,
-    items: 1
+    smartSpeed: 1000, // Время движения слайда
+    autoplayTimeout: 2000, // Время смены слайда
+    items: 1,
+    touchDrag: true
   });
 });
+
+$(window).resize(function () { // запускаем при каждом ресайзе окна
+  checkMedia();
+});
+
+function checkMedia() {
+  if (window.matchMedia('(max-width: 768px)').matches) {
+    $('.popular__gallery').owlCarousel({
+      loop: true, // Зацикливаем слайдер
+      margin: 0, // Отступ от элемента справа в 50px
+      nav: false, // Отключение навигации
+      autoplay: true, // Автозапуск слайдера
+      smartSpeed: 1000, // Время движения слайда
+      autoplayTimeout: 2000, // Время смены слайда
+      items: 1,
+      touchDrag: true
+    });
+  }
+}
